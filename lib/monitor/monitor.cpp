b05bef55b9e39ec3cf2b59e81dbafce59f0a3652
vanceb <vance@axxe.co.uk>
2022-11-14 10:42:05 +0000

    Fixed weekly status message and repeated notification about Pi lost

--- lib/monitor/monitor.cpp
+++ lib/monitor/monitor.cpp
@@ -350,6 +350,7 @@ void monitorTask(void * pvParameters) {
         if (pi_connected) {
             /* We can see the physical connection */
             if (pi_ok) {
+                no_pi = false;
                 /* All good! */
                 if (!pi_ok_prev) {
                     /* Heartbeat just come up */
@@ -373,7 +374,8 @@ void monitorTask(void * pvParameters) {
                     /* Startup or not seen in a while, stay silent */
                 }
             }
-        } else {
+        } else if (!no_pi) {
+            no_pi = true;
             if (pi_connected_prev) {
                 /* We have seen it up a moment ago */
                 log_e("Can't detect Raspberry Pi any more");
@@ -383,7 +385,6 @@ void monitorTask(void * pvParameters) {
 #endif  // FEATURE_PUSHOVER
             } else if(!no_pi) {
                 log_e("No Raspberry Pi detected on startup");
-                no_pi = true;
 #if FEATURE_PUSHOVER
                 pushover.send("rLabDoor Pi", "Raspberry Pi physical connection not detected", -1);
 #endif  // FEATURE_PUSHOVER
